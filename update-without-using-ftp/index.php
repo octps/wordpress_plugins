4dd0af22bc1efc7da6718ff162ab9a24455f80ea
octps <renmo9@gmail.com>
2017-11-23 16:37:36 +0900

    #1 ABSPATHチェック追加

--- update-without-using-ftp/index.php
+++ update-without-using-ftp/index.php
@@ -23,6 +23,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+if (!defined('ABSPATH')) {
+ exit;	
+}
 
 function set_fs_method($args) {
 	return 'direct';
